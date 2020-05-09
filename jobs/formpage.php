6f34d4e395a07bef23f535af661b1aff89d654ec
Amit Kumar <35621620+systronics@users.noreply.github.com>
2020-05-09 12:55:26 +0530

    Update formpage.php

--- jobs/formpage.php
+++ jobs/formpage.php
@@ -1,8 +1,9 @@
+---
+layout: post
+title: Jobs
+---
 
-<link rel="stylesheet" href="css/bootstrap.min.css">
-<div class="container">
-<h1 class="text-center">Contact me-</h1>
-
+<p><strong>Contact me-</strong></p>
 <hr>
 	<?php 
 		if(isset($_POST['sendmail'])) {
@@ -26,57 +27,4 @@
 
 			$mail->addReplyTo(EMAIL);
 			// print_r($_FILES['file']); exit;
-			for ($i=0; $i < count($_FILES['file']['tmp_name']) ; $i++) { 
-				$mail->addAttachment($_FILES['file']['tmp_name'][$i], $_FILES['file']['name'][$i]);    // Optional name
-			}
-			$mail->isHTML(true);                                  // Set email format to HTML
-
-			$mail->Subject = $_POST['subject'];
-			$mail->Body    = '<div style="border:2px solid red;">This is the HTML message body <b>in bold!</b></div>';
-			$mail->AltBody = $_POST['message'];
-
-			if(!$mail->send()) {
-			    echo 'Message could not be sent.';
-			    echo 'Mailer Error: ' . $mail->ErrorInfo;
-			} else {
-			    echo 'Message has been sent';
-			}
-		}
-	 ?>
-	<div class="row">
-    <div class="col-md-9 col-md-offset-2">
-        <form role="form" method="post" enctype="multipart/form-data">
-        	<div class="row">
-                <div class="col-sm-9 form-group">
-                    <label for="email">To Email:</label>
-                    <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email" maxlength="50">
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-sm-9 form-group">
-                    <label for="subject">Subject:</label>
-                    <input type="text" class="form-control" id="subject" name="subject" value="Test Mail with attachments" maxlength="50">
-                </div>
-            </div>
-            
-            <div class="row">
-                <div class="col-sm-9 form-group">
-                    <label for="name">Message:</label>
-                    <textarea class="form-control" type="textarea" id="message" name="message" placeholder="Your Message Here" maxlength="6000" rows="4">Test mail using PHPMailer</textarea>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-9 form-group">
-                    <label for="name">File:</label>
-                    <input name="file[]" multiple="multiple" class="form-control" type="file" id="file">
-                </div>
-            </div>
-             <div class="row">
-                <div class="col-sm-9 form-group">
-                    <button type="submit" name="sendmail" class="btn btn-lg btn-success btn-block">Send</button>
-                </div>
-            </div>
-        </form>
-	</div>
-</div>
+			for ($i=0; $i < count($_
