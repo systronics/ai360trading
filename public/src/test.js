a7685db0f354d27a4803c706143a5c7e690e8789
Amit Kumar <35621620+systronics@users.noreply.github.com>
2020-05-13 11:42:52 +0530

    Delete test.js

--- public/src/test.js
+++
@@ -1,7 +0,0 @@
-﻿var savePageWE=require('/public/src/nodeSavePageWE');
-
-
-savePageWE.scrape({ url: "/public/Intraday.mht", path: "intraday.html" }).then(function ()
-{
-    console.log("ok");
-});
